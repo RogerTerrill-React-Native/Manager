fdeb859c6c3d928f82344f47d82a2353c0f37959
Roger Terrill <rchicasterill@gmail.com>
2018-11-27 13:55:59 -0800

    Making LoginUser More Robust

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
-import firebase from '@firebase/app';
+import firebase from 'firebase';
 
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 
 export const emailChanged = text => {
   return {
@@ -22,7 +22,7 @@ export const loginUser = ({ email, password }) => {
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then(user => {
-        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
       });
   };
 };

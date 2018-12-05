ef9f6e22c91bd64adcc110cdcf1ceb27a5e0078e
Roger Terrill <rchicasterill@gmail.com>
2018-12-05 15:05:28 -0800

    Displaying Multiple Scenes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import Router from './router';
+import Router from './Router';
 
 class App extends Component {
   componentWillMount() {
--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import LoginForm from '../components/LoginForm';
+import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RootStack = createStackNavigator(
   {
-    Home: LoginForm
+    Home: LoginForm,
+    EmpList: EmployeeList
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Home'
   }
 );
 
 const AppContainer = createAppContainer(RootStack);
-
 
 class Router extends React.Component {
   render() {
